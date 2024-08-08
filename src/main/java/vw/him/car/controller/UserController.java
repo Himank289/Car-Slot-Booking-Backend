c948d952b08b605864160bfb2f13ef48d4de356d
Himank289 <tyagihimank28@gmail.com>
2024-08-08 12:58:42 +0530

    Controllers clean code added

--- src/main/java/vw/him/car/controller/UserController.java
+++ src/main/java/vw/him/car/controller/UserController.java
@@ -39,8 +39,6 @@ public class UserController {
         }
 
     }
-
-    // try git
     @DeleteMapping("user/{id}")
     public ResponseEntity<String>  deleteUser(@RequestHeader("Authorization") String jwt, @PathVariable long id) {
         try{
